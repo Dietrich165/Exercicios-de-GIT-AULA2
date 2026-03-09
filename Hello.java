730baa8ab7df77d4c86dba901a81dd260ea35360
Dietrich165 <gdietrich@minha.fag.edu.br>
2026-03-09 16:10:50 -0300

    Hello Word

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        helloWorld();
+    }
+
+    public static void helloWorld() {
+        System.out.println("Hello World");
+    }
+}
